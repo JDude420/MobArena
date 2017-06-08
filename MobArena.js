09081fdd733a11499261d35f06a1b860a423a32e
JDude420 <joshmcneely04@gmail.com>
2017-06-08 15:37:08 -0500

    Change v0.1b1
    +Link for first runners

--- MobArena.js
+++ MobArena.js
@@ -1,1 +1,8 @@
 //Start
+function newLevel(){
+  var fr=0
+  fr=ModPE.loadData(firstrun);
+  if(fr==0){msg("Thank youo for installing the MobArena Modification for MobArena map by RainbowPartyPlay");}
+}
+function msg(m){clientMessage("§6MobArenaModification§f > §7"+m);}
+function leaveGame(){ModPE.saveData(firstrun, 1);}

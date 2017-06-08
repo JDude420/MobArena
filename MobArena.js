0efcfe1e79572ee76a4f0d3562567f28943e3059
JDude420 <joshmcneely04@gmail.com>
2017-06-08 15:44:27 -0500

    Change v0.1b2
    +Hopeful fix for bug

--- MobArena.js
+++ MobArena.js
@@ -1,7 +1,7 @@
 //Start
 function newLevel(){
   var fr=0
-  fr=ModPE.loadData(firstrun);
+  fr=ModPE.loadData(firstrun)
   if(fr==0){msg("Thank youo for installing the MobArena Modification for MobArena map by RainbowPartyPlay");}
 }
 function msg(m){clientMessage("§6MobArenaModification§f > §7"+m);}
